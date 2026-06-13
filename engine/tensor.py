8a472d18bb9fccbecac5bc2d810060b0bb9df479
duaasiraj <duaasiraj37@gmail.com>
2026-06-13 21:26:10 +0500

    forward propogation done with basic add/mul

---
+++ engine/tensor.py
@@ -0,0 +1,19 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+class Tensor:
+    def __init__(self, data,_children=(),_op=''):
+        self.data=data
+        self.grad=0
+        self._prev=set(_children)#stores how to get back to this node 
+        self._op=_op #stores what operation produced this node
+        self._backward=lambda:None #topological sort 
+    
+    def __add__(self, other):
+       x=Tensor(self.data+other.data,(self,other),'+')
+       return x
+    
+    def __mul__(self, other):
+        x=Tensor(self.data*other.data,(self,other),'*')
+        return x
